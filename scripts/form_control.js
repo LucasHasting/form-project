b79e43138ba6f32b238bf5001ff179babbbd912f
LucasHasting <110124874+LucasHasting@users.noreply.github.com>
2023-10-31 20:24:40 -0500

    Update form_control.js
    temp fix

--- scripts/form_control.js
+++ scripts/form_control.js
@@ -5,6 +5,18 @@ async function start() {
   const response = await fetch(data_path);
   let data = await response.text();
   data = data.split("\r\n");
+
+  // used until data.txt works in github
+  data = ["jdoe@jdoe.com",
+    "(999)-999-9999",
+    "John Doe",
+    "1234",
+    "0",
+    "9",
+    "0",
+    "cool web site",
+    "1"]
+    
   jquery_functions(data);
 }
 
